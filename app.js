31e3f219cd88f30b79c5ff0172ed546968325bca
usmonovdev <usmonovazizbek15@gmail.com>
2022-06-30 18:30:34 +0500

    not working

---
+++ app.js
@@ -0,0 +1,8 @@
+function gen() {
+    var input = document.getElementById("inp")
+    if (input.length = "ayol") {
+        document.getElementById("img").src = "img/a.jpg"
+    } else {
+        document.getElementById("img").src = "img/v.jpg"
+    }
+}
